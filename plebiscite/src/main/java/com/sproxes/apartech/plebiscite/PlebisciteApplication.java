bc3f8ca856ca84f49384dc00b1d2de7ef696dc19
Ramkiran Tanniru <ramkiran.t06@gmail.com>
2021-03-28 18:38:36 +0530

    Initial commit of plebiscite

---
+++ plebiscite/src/main/java/com/sproxes/apartech/plebiscite/PlebisciteApplication.java
@@ -0,0 +1,13 @@
+package com.sproxes.apartech.plebiscite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlebisciteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlebisciteApplication.class, args);
+	}
+
+}
---
+++ plebiscite/src/test/java/com/sproxes/apartech/plebiscite/PlebisciteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sproxes.apartech.plebiscite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlebisciteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
